4ac2964104a6f92907f4820748afd973132aa8d3
boga5 <sunil.boga05@gmail.com>
2023-10-12 15:03:58 +0530

    Playing with capture groups

--- PowerShell/iterating-through-csv.ps1
+++ PowerShell/iterating-through-csv.ps1
@@ -18,10 +18,11 @@ foreach ($user in $csv) {
 
 Write-Host "`n Iterating thorugh CSV till particular value inside CSV column`n"
 try {
-    if ($csv) {
+    if ($csv) { 
+    #if there's no CSV it'll display the same msg on the screen
         foreach ($user in $csv) {
             if ( $user.name -eq 'padma' ) {
-                Write-Host "`n The user wants to end this`nWe ended it" -ErrorAction Stop
+                Write-Host "`n The user wants to end this`n`n So we ended it" -ErrorAction Stop
                 Break
             }
             else {
@@ -34,7 +35,15 @@ try {
     }
 } 
 catch {
-    Write-Output "`n Well, IF YOU'RE SEEING THIS.`n`n`n`n`n`nBelieve Me`n`n`n`nSomething's messed up`n"
+    Write-Output "`n Well, IF YOU'RE SEEING THIS,`n`n`n`n`n`nBelieve Me`n`n`n`nSomething's messed up`n"
     Write-Output $Error
     $Error.Clear()
+}
+
+Write-Host "`n Assigning the csv values to variables and then using them.`n"
+foreach ($user in $csv) {
+    $user_id = $user.id
+    $user_name = $user.name
+    $user_age = $user.age
+    Write-Host "ID $user_id is assigned to $user_name whose age is $user_age"
 }
